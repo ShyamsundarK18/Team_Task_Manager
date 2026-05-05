6077e63d43d23b22521842a432e98484cebb158b
Shyam-7865 <engr.shyamkumar7@gmail.com>
2026-05-05 19:27:26 +0530

    Project completed

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 
 
